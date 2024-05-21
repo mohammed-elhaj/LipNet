71f295088770cbb25264f298db04854ed51c66d3
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-05-22 00:18:43 +0200

    Update modelutil.py

--- app/modelutil.py
+++ app/modelutil.py
@@ -29,8 +29,7 @@ def load_model() -> Sequential:
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
    # model.load_weights(os.path.join('..','models','checkpoint'))
-    #checkpoint_dir = os.path.join('..', 'models')
-    checkpoint_dir = os.path.join('workspaces', 'LipNet', 'models', 'checkpoint.data-00000-of-00001')
+    checkpoint_dir = os.path.join('..', 'models')
 
     # Create a checkpoint instance that points to the folder where the checkpoints are saved
     checkpoint = tf.train.Checkpoint(model=model)
