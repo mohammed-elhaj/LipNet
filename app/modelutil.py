2c68349f260ba104c5a569f9987ae57a2f9ebe28
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-05-22 00:51:34 +0200

    Update modelutil.py

--- app/modelutil.py
+++ app/modelutil.py
@@ -28,7 +28,7 @@ def load_model() -> Sequential:
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
-    model.load_weights(os.path.join('..','models','checkpoint'))
+    model.load_weights(os.path.join('..','models','checkpoint.'))
     
     # checkpoint_dir = os.path.join('..', 'models')
     # checkpoint = tf.train.Checkpoint(model=model)
